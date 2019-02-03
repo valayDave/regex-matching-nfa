19a20ae786684ba484365c0364fa0a5b867225c9
Valay <valaygaurang@gmail.com>
2019-02-03 06:33:37 -0700

    new test added to shell script

--- Serial/run.sh
+++ Serial/run.sh
@@ -1,5 +1,5 @@
 g++ nfa-builder.cc -o search 
-./search "(l*)+(o*)" haiku0.txt haiku2.txt haiku3.txt
+./search "(l*o)+(o*)" haiku0.txt haiku2.txt haiku3.txt
 
 # ./search "(l|o)*" haiku0.txt # haiku2.txt haiku3.txt 
 # ./search "(lo)*" haiku0.txt # haiku2.txt haiku3.txt
